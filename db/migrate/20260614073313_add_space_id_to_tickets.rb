a8889e09cdb65dc45707570a4f1c5320fcf7fec6
Jose Mario <josemarioarias12@gmail.com>
2026-06-14 02:00:56 -0600

    fix: correct corrupted migration and AppLayout imports in Facilities pages

--- db/migrate/20260614073313_add_space_id_to_tickets.rb
+++ db/migrate/20260614073313_add_space_id_to_tickets.rb
@@ -1,8 +1,5 @@
-ruby -i -e '
-content = File.read(ARGV[0])
-content.sub!(
-  "  belongs_to :sla_policy,  optional: true",
-  "  belongs_to :sla_policy,  optional: true\n  belongs_to :space,        optional: true"
-)
-File.write(ARGV[0], content)
-' app/models/ticket.rb
+class AddSpaceIdToTickets < ActiveRecord::Migration[8.1]
+  def change
+    add_reference :tickets, :space, null: true, foreign_key: true, index: true
+  end
+end
